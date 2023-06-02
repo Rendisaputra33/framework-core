fd5a7f0a07fe1115d0e77d2e3437c8dd29383f7b
Rendisaputra33 <gorengmbah@gmail.com>
2023-06-03 00:16:41 +0700

    update helper

--- src/Helper/index.php
+++ src/Helper/index.php
@@ -15,3 +15,37 @@ if (!function_exists('env')) {
         return $_ENV[$name];
     }
 }
+
+if (!function_exists('session')) {
+    function session(string $name): mixed 
+    {
+        if (isset($_SESSION[$name])) return $_SESSION[$name];
+        return null;
+    }
+}
+
+if (!function_exists('setSession')) {
+    function setSession(string $name, mixed $value): void 
+    {
+        $_SESSION[$name] = $value;
+    }
+}
+
+if (!function_exists('message')) {
+    function message(string $name): mixed 
+    {
+        if (isset($_SESSION[$name])) {
+            $value = $_SESSION[$name];
+            unset($_SESSION[$name]);
+            return $value;
+        }
+        return null;
+    }
+}
+
+if (!function_exists('setMessage')) {
+    function setMessage(string $name, mixed $value): void 
+    {
+        $_SESSION[$name] = $value;
+    }
+}
